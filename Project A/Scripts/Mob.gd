e84834668889dc740bd3c9c25391b51d60e69fef
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-12-12 04:54:47 +0000

    test gameplay concept, not working corectly

---
+++ Project A/Scripts/Mob.gd
@@ -0,0 +1,10 @@
+extends RigidBody2D
+
+export var min_speed = 150
+export var max_speed = 250
+
+func _ready():
+	pass
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
---
+++ Project A/Scripts/Node.gd
@@ -0,0 +1,43 @@
+extends Node
+
+export (PackedScene) var MobScene
+var score
+
+func _ready():
+	randomize()
+	
+	# Start the game
+	if Input.is_action_pressed("ui_select"):
+		new_game()
+	
+# New game
+func new_game():
+	score = 0
+	$Player.start()
+	$Player/GameOver.visible = false
+
+func _game_over():
+	$MobTimer.stop()
+	$Player/GameOver.visible = true
+
+func _on_MobTimer_timeout():
+	# Chosing a random location on the path
+	$MobSpawner/MobLocation.offset = randi()
+	
+	# Create a mob instance and add it as a child
+	var mob = MobScene.instace()
+	add_child(mob)
+	
+	# Set the mob's location perpendicular to the path
+	var direction = $MobSpawner/MobLocation.rotation + PI / 2
+	
+	# Set the mob's position to a random location
+	mob.position = $MobSpawner/MobLocation.position
+	
+	# Add some randomness to the direction
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	# Set the velocity
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
