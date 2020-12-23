1074bb3ce1733cb96b935ef8cb40b36a19e262f5
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-12-23 06:04:20 +0000

    Player and Enemy features

--- Project A/Scripts/BreadEnemy.gd
+++ Project A/Scripts/BreadEnemy.gd
@@ -1,10 +1,19 @@
 extends "res://Scripts/Humanoid.gd"
 
 func _ready():
+	set_physics_process(false)
 	velocity.x = -speed.x
+
+func _on_StompArea_body_entered(body):
+	if body.global_position.y > $StompArea.global_position.y:
+		return
+	$CollisionShape2D.disabled = true
+	queue_free()
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	if is_on_wall():
 		velocity.x *= -1.0
 	velocity.y = move_and_slide(velocity, FLOOR_NORMAL).y
+
+
--- Project A/Scripts/Player-physics.gd
+++ Project A/Scripts/Player-physics.gd
@@ -1,5 +1,15 @@
 extends Humanoid
 
+onready var player_sprite = $Sprite
+
+export var stomp_impulse = 1000
+
+func _on_EnemyDetector_area_entered(area):
+	velocity = calculate_stomp_velocity(velocity, stomp_impulse)
+
+func _on_EnemyDetector_body_entered(body):
+	queue_free()
+
 func _physics_process(delta):
 	var _is_jump_interrupted = Input.is_action_just_released("move_up") and velocity.y < 0.0
 	var direction = get_direction()
@@ -9,7 +19,7 @@ func _physics_process(delta):
 func _process(delta):
 	# Flip sprite
 	if velocity.x != 0:
-		$Sprite.flip_h = velocity.x < 0
+		player_sprite.flip_h = velocity.x < 0
 	
 func get_direction():
 	 return Vector2(
@@ -26,3 +36,7 @@ func calculate_move_velocity(velocity, direction, speed, _is_jump_interrupted):
 		new_velocity.y = 0.0
 	return new_velocity
 	
+func calculate_stomp_velocity(velocity, impulse):
+	var _velocity = velocity
+	_velocity.y = -impulse
+	return _velocity
