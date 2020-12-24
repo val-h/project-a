60f86149d0f6fdf008028be039315789876e8818
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-12-24 04:19:06 +0000

    extended level with mroe enemies, a lot of bugs ;d

--- Project A/Scripts/BreadEnemy.gd
+++ Project A/Scripts/BreadEnemy.gd
@@ -1,12 +1,14 @@
 extends "res://Scripts/Humanoid.gd"
 
 func _ready():
-	set_physics_process(false)
+	# For VisibilityNotifier, doesn't work properly
+	# nvm the problem is not from it ;d
+	#set_physics_process(false)
 	velocity.x = -speed.x
 
 func _on_StompArea_body_entered(body):
-	if body.global_position.y > $StompArea.global_position.y:
-		return
+#	if body.global_position.y > $StompArea.global_position.y:
+#		 return
 	$CollisionShape2D.disabled = true
 	queue_free()
 
@@ -15,5 +17,3 @@ func _physics_process(delta):
 	if is_on_wall():
 		velocity.x *= -1.0
 	velocity.y = move_and_slide(velocity, FLOOR_NORMAL).y
-
-
--- Project A/Scripts/Humanoid.gd
+++ Project A/Scripts/Humanoid.gd
@@ -3,7 +3,7 @@ class_name Humanoid
 
 const FLOOR_NORMAL = Vector2.UP
 
-export var speed: = Vector2(300.0, 800.0)
+export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 2500.0
 
 export var velocity: = Vector2()
